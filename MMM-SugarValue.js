163ae2e38d5842fef88505b3c7606f5d1e016e94
Neil Balharrie <neil@balharrie.co.uk>
2020-01-05 15:38:39 +0000

    First releae

---
+++ MMM-SugarValue.js
@@ -0,0 +1,145 @@
+(function () {
+    'use strict';
+
+    var DexcomTrend;
+    (function (DexcomTrend) {
+        DexcomTrend[DexcomTrend["NONE"] = 0] = "NONE";
+        DexcomTrend[DexcomTrend["DOUBLE_UP"] = 1] = "DOUBLE_UP";
+        DexcomTrend[DexcomTrend["SINGLE_UP"] = 2] = "SINGLE_UP";
+        DexcomTrend[DexcomTrend["FORTYFIVE_UP"] = 3] = "FORTYFIVE_UP";
+        DexcomTrend[DexcomTrend["FLAT"] = 4] = "FLAT";
+        DexcomTrend[DexcomTrend["FORTYFIVE_DOWN"] = 5] = "FORTYFIVE_DOWN";
+        DexcomTrend[DexcomTrend["SINGLE_DOWN"] = 6] = "SINGLE_DOWN";
+        DexcomTrend[DexcomTrend["DOUBLE_DOWN"] = 7] = "DOUBLE_DOWN";
+        DexcomTrend[DexcomTrend["NOT_COMPUTABLE"] = 8] = "NOT_COMPUTABLE";
+        DexcomTrend[DexcomTrend["RATE_OUT_OF_RANGE"] = 9] = "RATE_OUT_OF_RANGE";
+    })(DexcomTrend || (DexcomTrend = {}));
+
+    var ModuleNotification;
+    (function (ModuleNotification) {
+        ModuleNotification["CONFIG"] = "CONFIG";
+        ModuleNotification["DATA"] = "DATA";
+        ModuleNotification["ALL_MODULES_STARTED"] = "ALL_MODULES_STARTED";
+    })(ModuleNotification || (ModuleNotification = {}));
+
+    Module.register("MMM-Dexcom", {
+        defaults: {
+            "usServerUrl": "https://share1.dexcom.com",
+            "euServerUrl": "https://shareous1.dexcom.com",
+            "server": "us",
+            "updateSecs": 300,
+            "units": "mmol"
+        },
+        message: "Loading...",
+        reading: undefined,
+        getDom: function () {
+            var wrapper = document.createElement("div");
+            if (this.message !== undefined) {
+                wrapper.innerText = this.message;
+            }
+            else if (this.reading == undefined) {
+                wrapper.innerText = "Reading not available";
+            }
+            else if (this.config !== undefined) {
+                console.log(this.reading);
+                var sugar = document.createElement("span");
+                sugar.className = "bright medium";
+                if (this.config.units === "mg") {
+                    sugar.innerText = this.reading.sugarMl.toString() + " mg/dl";
+                }
+                else {
+                    sugar.innerText = this.reading.sugarMl.toString() + " mmol/l";
+                }
+                var trend = document.createElement("span");
+                trend.className = "dimmed medium";
+                trend.className = "fa fa-fw ";
+                switch (this.reading.trend) {
+                    case DexcomTrend.DOUBLE_DOWN:
+                        trend.className += "fa-angle-double-down";
+                        break;
+                    case DexcomTrend.DOUBLE_UP:
+                        trend.className += "fa-angle-double-up";
+                        break;
+                    case DexcomTrend.FLAT:
+                        trend.className += "fa-angle-right";
+                        break;
+                    case DexcomTrend.FORTYFIVE_DOWN:
+                        trend.className += "fa-location-arrow fa-rotate-90";
+                        break;
+                    case DexcomTrend.FORTYFIVE_UP:
+                        trend.className += "fa-location-arrow";
+                        break;
+                    case DexcomTrend.NONE:
+                        break;
+                    case DexcomTrend.NOT_COMPUTABLE:
+                        trend.className += "fa-question-circle";
+                        break;
+                    case DexcomTrend.RATE_OUT_OF_RANGE:
+                        trend.className += "fa-exclamation-triangle";
+                        break;
+                    case DexcomTrend.SINGLE_DOWN:
+                        trend.className += "fa-angle-down";
+                        break;
+                    case DexcomTrend.SINGLE_UP:
+                        trend.className += "fa-angle-up";
+                        break;
+                }
+                wrapper.appendChild(trend);
+                wrapper.appendChild(sugar);
+            }
+            return wrapper;
+        },
+        start: function () {
+            console.log("Starting");
+            var config = this.config;
+            if (config == undefined) {
+                this.message = "Configuration is not defined";
+            }
+            else {
+                config.serverUrl = config.server === "us" ? config.usServerUrl : config.euServerUrl;
+                if (config.username === undefined || config.password === undefined) {
+                    this.message = "Username or password not configured";
+                }
+                else {
+                    this.message = this.message;
+                }
+            }
+            this._updateDom();
+        },
+        notificationReceived: function (notification, payload, sender) {
+            if (notification === "ALL_MODULES_STARTED") {
+                this._sendSocketNotification(ModuleNotification.CONFIG, { config: this.config });
+            }
+        },
+        socketNotificationReceived: function (notification, payload) {
+            console.log(notification, payload);
+            if (notification === ModuleNotification.DATA) {
+                var apiResponse = payload.apiResponse;
+                if (apiResponse !== undefined) {
+                    if (apiResponse.error !== undefined) {
+                        this.message = apiResponse.error.message + ":" + apiResponse.error.statusCode;
+                    }
+                    else {
+                        this.reading = apiResponse.readings.length > 0 ? apiResponse.readings[0] : undefined;
+                        this.message = undefined;
+                    }
+                    this._updateDom();
+                }
+            }
+        },
+        _sendSocketNotification: function (notification, payload) {
+            if (this.sendSocketNotification !== undefined) {
+                this.sendSocketNotification(notification, payload);
+            }
+            else {
+                console.error("sendSocketNotification is not present");
+            }
+        },
+        _updateDom: function () {
+            if (this.updateDom !== undefined) {
+                this.updateDom();
+            }
+        }
+    });
+
+}());
---
+++ node_helper.js
@@ -0,0 +1,145 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('request'), require('qs')) :
+    typeof define === 'function' && define.amd ? define(['request', 'qs'], factory) :
+    (global = global || self, factory(global.request, global.qs));
+}(this, (function (request, qs) { 'use strict';
+
+    request = request && request.hasOwnProperty('default') ? request['default'] : request;
+
+    var DexcomReadingImpl = /** @class */ (function () {
+        function DexcomReadingImpl(raw) {
+            var dateMatch = raw.WT.match(/\((.*)\)/);
+            this.date = dateMatch === null || dateMatch.length == 0 ? undefined : new Date(parseInt(dateMatch[1]));
+            this.sugar = raw.Value;
+            this.sugarMl = Math.floor(10 * (raw.Value / 18.0)) / 10;
+            this.trend = raw.Trend;
+        }
+        return DexcomReadingImpl;
+    }());
+
+    var DexcomApiImpl = /** @class */ (function () {
+        function DexcomApiImpl(server, username, password) {
+            this._server = server;
+            this._username = username;
+            this._password = password;
+        }
+        DexcomApiImpl.prototype.doPost = function (uri, body, callback) {
+            var bodyAsString = body == undefined ? "" : JSON.stringify(body);
+            return request({
+                uri: uri,
+                method: "POST",
+                headers: {
+                    'User-Agent': DexcomApiImpl.AGENT,
+                    'Content-Type': DexcomApiImpl.CONTENT_TYPE,
+                    'Content-Length': bodyAsString == undefined ? 0 : bodyAsString.length,
+                    'Accept': DexcomApiImpl.ACCEPT
+                },
+                body: bodyAsString
+            }, callback);
+        };
+        DexcomApiImpl.prototype.login = function (callback) {
+            return this.doPost(this._server + "/ShareWebServices/Services/General/LoginPublisherAccountByName", {
+                "accountName": this._username,
+                "password": this._password,
+                "applicationId": DexcomApiImpl.APPLICATION_ID
+            }, callback);
+        };
+        DexcomApiImpl.prototype.fetchLatest = function (sessionId, maxCount, minutes, callback) {
+            return this.doPost(this._server + "/ShareWebServices/Services/Publisher/ReadPublisherLatestGlucoseValues?" + qs.stringify({
+                sessionID: sessionId,
+                minutes: minutes === undefined ? 1440 : Math.max(1, minutes),
+                maxCount: maxCount === undefined ? 1 : Math.max(1, maxCount),
+            }), undefined, callback);
+        };
+        DexcomApiImpl.prototype.fetchData = function (callback, maxCount, minutes) {
+            var _this = this;
+            this.login(function (error, response, body) {
+                if (error != null || response.statusCode !== 200) {
+                    callback({
+                        error: {
+                            statusCode: response == undefined ? -1 : response.statusCode,
+                            message: "Failed to login"
+                        },
+                        readings: []
+                    });
+                }
+                else {
+                    var sessionId = body.substring(1, body.length - 1);
+                    _this.fetchLatest(sessionId, maxCount, minutes, function (_error, _response, body) {
+                        if (error != null || response.statusCode !== 200) {
+                            callback({
+                                error: {
+                                    statusCode: response == undefined ? -1 : response.statusCode,
+                                    message: "Failed to fetch readings"
+                                },
+                                readings: []
+                            });
+                        }
+                        else {
+                            var rawReadings = JSON.parse(body);
+                            callback({
+                                error: undefined,
+                                readings: rawReadings.map(function (reading) { return new DexcomReadingImpl(reading); })
+                            });
+                        }
+                    });
+                }
+            });
+        };
+        DexcomApiImpl.APPLICATION_ID = "d89443d2-327c-4a6f-89e5-496bbb0317db";
+        DexcomApiImpl.AGENT = "Dexcom Share/3.0.2.11 CFNetwork/711.2.23 Darwin/14.0.0";
+        DexcomApiImpl.CONTENT_TYPE = "application/json";
+        DexcomApiImpl.ACCEPT = "application/json";
+        return DexcomApiImpl;
+    }());
+    function DexcomApiFactory(server, username, password) {
+        return new DexcomApiImpl(server, username, password);
+    }
+
+    var ModuleNotification;
+    (function (ModuleNotification) {
+        ModuleNotification["CONFIG"] = "CONFIG";
+        ModuleNotification["DATA"] = "DATA";
+        ModuleNotification["ALL_MODULES_STARTED"] = "ALL_MODULES_STARTED";
+    })(ModuleNotification || (ModuleNotification = {}));
+
+    var NodeHelper = require("node_helper");
+    module.exports = NodeHelper.create({
+        socketNotificationReceived: function (notification, payload) {
+            var _this = this;
+            switch (notification) {
+                case ModuleNotification.CONFIG:
+                    var config_1 = payload.config;
+                    if (config_1 !== undefined) {
+                        var api_1 = DexcomApiFactory(config_1.serverUrl, config_1.username, config_1.password);
+                        setTimeout(function () {
+                            _this.fetchData(api_1, config_1.updateSecs);
+                        }, 500);
+                    }
+                    break;
+            }
+        },
+        // stop: () => {
+        //     stopped = true;
+        // },
+        fetchData: function (api, updateSecs) {
+            var _this = this;
+            api.fetchData(function (response) {
+                _this._sendSocketNotification(ModuleNotification.DATA, { apiResponse: response });
+                setTimeout(function () {
+                    _this.fetchData(api, updateSecs);
+                }, updateSecs * 1000);
+            }, 1);
+        },
+        _sendSocketNotification: function (notification, payload) {
+            console.log("Sending", notification, payload);
+            if (this.sendSocketNotification !== undefined) {
+                this.sendSocketNotification(notification, payload);
+            }
+            else {
+                console.error("sendSocketNotification is not present");
+            }
+        },
+    });
+
+})));
