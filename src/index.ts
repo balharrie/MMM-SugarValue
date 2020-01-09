c1ed13270dc33cf3f935df4a2feb4c5f3b1aee9a
Neil Balharrie <neil@balharrie.co.uk>
2020-01-09 20:36:49 +0000

    Display time using momentjs and change icons.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { DexcomReading, DexcomTrend, DexcomApiResponse } from "./dexcom";
 import { Config } from "./Config";
 import { NotificationPayload } from "./NotificationPayload";
 import { ModuleNotification } from "./ModuleNotification";
+import moment from 'moment';
 
 interface MagicMirrorApi {
     config?: Config;
@@ -15,11 +16,13 @@ interface MagicMirrorOptions extends MagicMirrorApi {
     defaults: Config;
     message: string | undefined;
     reading: DexcomReading | undefined;
+    clockSpan: HTMLSpanElement | undefined;
 
     getDom: () => HTMLDivElement;
     start: () => void;
     _sendSocketNotification(notification: string, payload: NotificationPayload): void;
     _updateDom(): void;
+    _createIcon(className: string, rotate?: string): HTMLSpanElement;
 }
 
 interface MagicMirrorModule {
@@ -38,6 +41,7 @@ Module.register("MMM-SugarValue", {
     } as Config,
     message: "Loading...",
     reading: undefined,
+    clockSpan: undefined,
     getDom(): HTMLDivElement {
         const wrapper: HTMLDivElement = document.createElement("div");
         if (this.message !== undefined) {
@@ -49,7 +53,7 @@ Module.register("MMM-SugarValue", {
             const date: HTMLDivElement = document.createElement("div");
 
             if (this.reading.date !== undefined) {
-                date.innerText = this.reading.date.toLocaleString();
+                date.innerText = moment(this.reading.date).fromNow();
                 date.className = "dimmed small";
             }
 
@@ -66,24 +70,24 @@ Module.register("MMM-SugarValue", {
             }
 
             const trend: HTMLSpanElement = document.createElement("span");
-            trend.className = "dimmed medium";
-            trend.className = "fa fa-fw ";
 
             switch (this.reading.trend) {
                 case DexcomTrend.DOUBLE_DOWN:
-                    trend.className += "fa-angle-double-down";
+                    trend.appendChild(this._createIcon("fa-arrow-down"));
+                    trend.appendChild(this._createIcon("fa-arrow-down"));
                     break;
                 case DexcomTrend.DOUBLE_UP:
-                    trend.className += "fa-angle-double-up";
+                    trend.appendChild(this._createIcon("fa-arrow-up"));
+                    trend.appendChild(this._createIcon("fa-arrow-up"));
                     break;
                 case DexcomTrend.FLAT:
-                    trend.className += "fa-angle-right";
+                    trend.appendChild(this._createIcon("fa-arrow-right"));
                     break;
                 case DexcomTrend.FORTYFIVE_DOWN:
-                    trend.className += "fa-location-arrow fa-rotate-90";
+                    trend.appendChild(this._createIcon("fa-arrow-right", "rotate-45"));
                     break;
                 case DexcomTrend.FORTYFIVE_UP:
-                    trend.className += "fa-location-arrow";
+                    trend.appendChild(this._createIcon("fa-arrow-up", "rotate-45"));
                     break;
                 case DexcomTrend.NONE:
                     break;
@@ -94,10 +98,10 @@ Module.register("MMM-SugarValue", {
                     trend.className += "fa-exclamation-triangle";
                     break;
                 case DexcomTrend.SINGLE_DOWN:
-                    trend.className += "fa-angle-down";
+                    trend.appendChild(this._createIcon("fa-arrow-down"));
                     break;
                 case DexcomTrend.SINGLE_UP:
-                    trend.className += "fa-angle-up";
+                    trend.appendChild(this._createIcon("fa-arrow-up"));
                     break;
             }
 
@@ -106,6 +110,7 @@ Module.register("MMM-SugarValue", {
             reading.appendChild(units);
             wrapper.appendChild(reading);
             wrapper.appendChild(date);
+            this.clockSpan = date;
         }
         return wrapper;
     },
@@ -123,6 +128,11 @@ Module.register("MMM-SugarValue", {
             }
         }
         this._updateDom();
+        setInterval(() => {
+            if (this.clockSpan !== undefined && this.reading !== undefined && this.reading.date !== undefined) {
+                this.clockSpan.textContent = moment(this.reading.date).fromNow();
+            }
+        }, 30000);
     },
     notificationReceived(notification: string, payload: any, sender: any): void {
         if (notification === "ALL_MODULES_STARTED") {
@@ -155,6 +165,14 @@ Module.register("MMM-SugarValue", {
         if (this.updateDom !== undefined) {
             this.updateDom();
         }
+    },
+    _createIcon(className: string, rotate?: string): HTMLSpanElement {
+        const icon:HTMLSpanElement = document.createElement("span");
+        icon.className = "fa fa-fw " + className;
+        if (rotate !== undefined) {
+            icon.setAttribute("data-fa-transform", rotate);
+        }
+        return icon;
     }
 });
 
